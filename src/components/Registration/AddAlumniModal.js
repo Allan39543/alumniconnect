e23d0375e78a3cffb1e1f02396d83620a18ab488
Allan39543 <warigiachegesa@gmail.com>
2023-11-04 21:41:26 +0400

    forms

--- src/components/Registration/AddAlumniModal.js
+++ src/components/Registration/AddAlumniModal.js
@@ -1,7 +1,37 @@
-import React from "react";
+import React,{useState} from "react";
 import {AiOutlineCloseCircle} from 'react-icons/ai'
+import axios from 'axios'
 
 function AddAlumniModal({closeModal}){
+
+    const[details,setDetails]=useState({email:"",names:"",userType:"",course:"",gradyr:"",loading:false})
+
+    const HandleSubmit = async (e) => {
+        e.preventDefault();
+        setDetails({ loading: true });
+    
+        const userDetails = {
+          email: details.email,
+          names: details.names,
+          course:details.course,
+          gradyr:details.gradyr,
+          type:details.userType
+          
+        };
+        try {
+          const response = await axios.post(
+            `${process.env.REACT_APP_API_URL}/api/v1/users/`,
+            userDetails
+          );
+          
+          setDetails({email:"",names:"",userType:"",course:"",gradyr:"",loading:false}); // Reset the comment input after successful submission
+        //   props.setCommentSent(prev => !prev); // Toggle commentSent to trigger re-fetching comments
+        } catch (error) {
+          setDetails({email:"",names:"",userType:"",course:"",gradyr:"",loading:false});
+        }
+      };
+
+      console.log(details)
 
     return(
         <div className="transparent-modal-wrapper">
@@ -23,26 +53,26 @@ function AddAlumniModal({closeModal}){
 
 <h1>Add Alumni</h1>
 
-                <form>
+                <form onSubmit={HandleSubmit}>
 
                 <div className="form-details">
                 <label> Email</label>
 
-                <input type="email" required/>
+                <input type="email" onChange={e => setDetails({ ...details, email: e.target.value })}  required/>
                 </div>
 
                 <div className="form-details">
                 <label> Names </label>
                 
 
-                <input type="text" required/>
+                <input type="text" onChange={e => setDetails({ ...details, names: e.target.value })}  required/>
                 </div>
 
                 <div className="form-details">
 
                 <label> User Type </label>
                 
-                <input type="text" required/>
+                <input type="text" onChange={e => setDetails({ ...details, userType: e.target.value })}  required/>
 
                 </div>
 
@@ -50,7 +80,7 @@ function AddAlumniModal({closeModal}){
 
                 <label> Course </label>
 
-                <input type="text" required/>
+                <input type="text" onChange={e => setDetails({ ...details, course: e.target.value })}  required/>
 
                 </div>
 
@@ -58,7 +88,7 @@ function AddAlumniModal({closeModal}){
 
                 <label> Grad Year </label>
 
-                <input type="text" required/>
+                <input type="text" onChange={e => setDetails({ ...details, gradyr: e.target.value })}  required/>
 
                 </div>
 
--- src/components/Registration/AddEvents.js
+++ src/components/Registration/AddEvents.js
@@ -1,7 +1,38 @@
-import React from "react";
+import React,{useState} from "react";
 import {AiOutlineCloseCircle} from 'react-icons/ai'
+import axios from 'axios'
 
 function AddEvents({closeModal}){
+
+    const[details,setDetails]=useState({title:"",displayName:"",venue:"",date:"",time:"",type:"",loading:false})
+
+    const HandleSubmit = async (e) => {
+        e.preventDefault();
+        setDetails({ loading: true });
+    
+        const eventDetails = {
+          title: details.title,
+          displayName: details.displayName,
+          date:details.date,
+          time:details.type,
+          type:details.type,
+          venue:details.venue
+          
+        };
+        try {
+          const response = await axios.post(
+            `${process.env.REACT_APP_API_URL}/api/v1/event/`,
+            eventDetails
+          );
+          
+          setDetails({ title:"",displayName:"",date:"",time:"",type:"",venue:"",loading:false }); // Reset the comment input after successful submission
+        //   props.setCommentSent(prev => !prev); // Toggle commentSent to trigger re-fetching comments
+        } catch (error) {
+          setDetails({ title:"",displayName:"",date:"",time:"",type:"",venue:"",loading:false });
+        }
+      };
+
+      console.log(details)
 
     return(
         <div className="transparent-modal-wrapper">
@@ -21,40 +52,47 @@ function AddEvents({closeModal}){
 
     <div className="add-object">
 
-<h1>Add Event</h1>
+    <h1>Add Event</h1>
 
-                <form>
+                <form onSubmit={HandleSubmit}>
 
                 <div className="form-details">
                 <label> Event Title</label>
 
-                <input type="email" required/>
+                <input type="text" onChange={e => setDetails({ ...details, title: e.target.value })} required/>
                 </div>
 
                 <div className="form-details">
                 <label> Event Display Name </label>
                 
 
-                <input type="text" required/>
+                <input type="text" onChange={e => setDetails({ ...details, displayName: e.target.value })}  required/>
+                </div>
+
+                <div className="form-details">
+                <label> Event Venue</label>
+                
+
+                <input type="text" onChange={e => setDetails({ ...details, venue: e.target.value })}  required/>
                 </div>
 
                 <div className="form-details">
                 <label> Date </label>
                 
 
-                <input type="date" id="dateInput" name="dateInput" />
+                <input type="date" id="dateInput" name="dateInput" onChange={e => setDetails({ ...details, date: e.target.value })}  />
                 </div>
 
                 <div className="form-details">
                 <label> Time </label>
-                <input type="time" id="timeInput" name="timeInput" />
+                <input type="time" id="timeInput" name="timeInput" onChange={e => setDetails({ ...details, time: e.target.value })}  />
                 </div>
 
   <div className="form-details">
                 <label> Event Type </label>
                 
 
-                    <select id="selectInput" name="selectInput">
+                    <select id="selectInput" name="selectInput" onChange={e => setDetails({ ...details, type: e.target.value })} >
         <option value="professional">Professional Development</option>
         <option value="networking">networking</option>
         <option value="campusevents">campus events</option>
