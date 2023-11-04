e4fdcbe6ea93860b315a0bc9c48d9323a13758bb
Allan39543 <warigiachegesa@gmail.com>
2023-11-04 19:19:33 +0400

    add event

--- src/components/Registration/AddEvents.js
+++ src/components/Registration/AddEvents.js
@@ -5,8 +5,7 @@ function AddEvents({closeModal}){
 
     return(
         <div className="transparent-modal-wrapper">
-
-<div className='close-modal' onClick={()=>closeModal(false)} >
+        <div className='close-modal' onClick={()=>closeModal(false)} >
 
 <div className='close-text'>
     Close
@@ -20,9 +19,44 @@ function AddEvents({closeModal}){
 
 <div className="form-wrapper">
 
-    <form>
-        
-    </form>
+    <div className="add-object">
+
+<h1>Add Event</h1>
+
+                <form>
+
+                <div className="form-details">
+                <label> Event Title</label>
+
+                <input type="email" required/>
+                </div>
+
+                <div className="form-details">
+                <label> Event Display Name </label>
+                
+
+                <input type="text" required/>
+                </div>
+
+  
+
+                <div className="form-details">
+
+                <label> Media </label>
+
+                <input type="file" required/>
+
+                </div>
+
+                <div className="form-details">
+                <button type="submit">
+                    Register
+                </button>
+                </div>
+
+            </form>
+
+    </div>
 
 
 </div>
