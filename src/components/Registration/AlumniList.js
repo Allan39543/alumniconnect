7276760bb1985013cf2ca3da7770b0e658f22244
Allan39543 <warigiachegesa@gmail.com>
2023-11-04 20:58:38 +0400

    tables

--- src/components/Registration/AlumniList.js
+++ src/components/Registration/AlumniList.js
@@ -1,5 +1,8 @@
 import React from "react";
 import {AiOutlineCloseCircle} from 'react-icons/ai'
+import { nanoid } from 'nanoid';
+import {GrUpdate} from 'react-icons/gr'
+import {AiTwotoneDelete} from 'react-icons/ai'
 
 function AlumniList({closeModal}){
 
@@ -19,6 +22,43 @@ function AlumniList({closeModal}){
 </div>
 
 <div className="form-wrapper" id="list-div">
+
+<table id="table">
+    <thead>
+        <tr>
+        <th>Email</th>
+        <th>Names</th>
+        <th>User Type</th>
+        <th>Course</th>
+        <th>Graduation Year</th>
+        <th>Update</th>
+        <th>Delete</th>
+        </tr>
+    </thead>
+
+
+    <tbody>
+       
+
+        <tr key={nanoid()}>
+
+            <td>warigiachegesa@gmail.com</td>
+            <td>HALLAN CHEGE WARIGIA</td>
+            <td>Admin</td>
+            <td>COMPUTER SCINECE</td>
+            <td>2024</td>
+            <td ><GrUpdate size="1.1em" className="center-td-content"/></td>
+            <td ><AiTwotoneDelete size="1.1em" color="red" className="center-td-content"/></td>
+            
+            
+            
+            </tr>
+        
+       
+    
+    </tbody>
+
+</table>
 
 </div>
 
