b9c933cf2f060334ea2910fa597d2b15071a21e1
Allan39543 <warigiachegesa@gmail.com>
2023-12-20 19:27:12 +0400

    footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,11 +1,14 @@
 import React from "react";
 
-function Footer(){
+function Footer() {
 
-    return(
+    return (
         <div className="footer">
-            
-            <p>AlumniConnect</p>
+
+            <p>Hallan Chege Warigia</p>
+            <p>&copy; 2023</p>
+            <p>Alumni Connect App</p>
+
 
         </div>
     )
