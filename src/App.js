14432f2e1ea6659616ce2fdd598dc31b1fd9b63a
Allan39543 <warigiachegesa@gmail.com>
2023-12-21 13:33:32 +0400

    updates

--- src/App.js
+++ src/App.js
@@ -22,41 +22,41 @@ function App() {
 
   const [user, setUser] = useState(null)
 
-  const getUser = async () => {
-		try {
+//   const getUser = async () => {
+// 		try {
       
 			
-			const url = `${process.env.REACT_APP_API_URL}/auth/login/success`;
-			const { data } = await axios.get(url, { withCredentials: true });
-		   setUser(data.user._json);
-    }
-    catch(err){
+// 			const url = `${process.env.REACT_APP_API_URL}/auth/login/success`;
+// 			const { data } = await axios.get(url, { withCredentials: true });
+// 		   setUser(data.user._json);
+//     }
+//     catch(err){
 
-console.log(err)
+// console.log(err)
 
-    }
-  }
-
-  useEffect(() => {
-		getUser();
-    
-	}, []);
-
-
-// useEffect(() => {
-  
-//   const searchParams = new URLSearchParams(location.search);
-//   const userData = searchParams.get("userData");
-
-  
-//   if (userData) {
-//     const userDataObject = JSON.parse(decodeURIComponent(userData));
-//     setUser(userDataObject);
-//     console.log("User data:", userDataObject);
-    
-    
+//     }
 //   }
-// }, [location.search]);
+
+//   useEffect(() => {
+// 		getUser();
+    
+// 	}, []);
+
+
+useEffect(() => {
+  
+  const searchParams = new URLSearchParams(location.search);
+  const userData = searchParams.get("userData");
+
+  
+  if (userData) {
+    const userDataObject = JSON.parse(decodeURIComponent(userData));
+    setUser(userDataObject);
+    console.log("User data:", userDataObject);
+    
+    
+  }
+}, [location.search]);
 
 
 console.log("user",user)
